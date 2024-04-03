c26380ddba26378ec9cc509a673d0938d3c980d7
LBarnes <l.alexisbarnes@gmail.com>
2024-04-03 02:49:29 -0300

    Fix

--- index.php
+++ index.php
@@ -1,9 +1,15 @@
-<?php
+<?
 include 'Application.php';
 
 $app = new Application();
 try{
     $app->startRandomTournament();
 } catch(Exception $e){
-    echo $e->getMessage();
+    //echo $e->getMessage();
 }
+?>
+<html>
+    <body>
+        Hi!
+    </body>
+</html>
