5484dd5c90881b7d133d377e2ae4c26280439221
LBarnes <l.alexisbarnes@gmail.com>
2024-04-03 06:45:04 -0300

    missin extra param

--- Classes/PlayersArray.php
+++ Classes/PlayersArray.php
@@ -15,9 +15,6 @@ class PlayersArray extends Player{
         }
         $first = true;
         foreach($player_list as $player_data){
-            if(!is_array($player_data)){
-                throw new Exception("Bad Array Format", 999);
-            }
             $name = $player_data["name"];
             if(!$gender){
                 $gender = $player_data["gender"];
--- index.php
+++ index.php
@@ -7,14 +7,14 @@
         <form action="">
             <p>Player List (JSON)</p>
             <textarea id="player_list" style="width: 632px; height: 152px;">
-[["name":"Test 0","gender":"M","handicap":24,"force":9,"velocity":4,"reaction":0],
-["name":"Test 1","gender":"M","handicap":11,"force":10,"velocity":8,"reaction":0],
-["name":"Test 2","gender":"M","handicap":74,"force":3,"velocity":1,"reaction":0],
-["name":"Test 3","gender":"M","handicap":80,"force":2,"velocity":3,"reaction":0],
-["name":"Test 4","gender":"M","handicap":99,"force":5,"velocity":5,"reaction":0],
-["name":"Test 5","gender":"M","handicap":97,"force":1,"velocity":10,"reaction":0],
-["name":"Test 6","gender":"M","handicap":90,"force":3,"velocity":8,"reaction":0],
-["name":"Test 7","gender":"M","handicap":64,"force":10,"velocity":7,"reaction":0]]</textarea>
+[{"name":"Test 0","gender":"M","extra":{"handicap":24,"force":9,"velocity":4,"reaction":0}},
+{"name":"Test 1","gender":"M","extra":{"handicap":11,"force":10,"velocity":8,"reaction":0}},
+{"name":"Test 2","gender":"M","extra":{"handicap":74,"force":3,"velocity":1,"reaction":0}},
+{"name":"Test 3","gender":"M","extra":{"handicap":80,"force":2,"velocity":3,"reaction":0}},
+{"name":"Test 4","gender":"M","extra":{"handicap":99,"force":5,"velocity":5,"reaction":0}},
+{"name":"Test 5","gender":"M","extra":{"handicap":97,"force":1,"velocity":10,"reaction":0}},
+{"name":"Test 6","gender":"M","extra":{"handicap":90,"force":3,"velocity":8,"reaction":0}},
+{"name":"Test 7","gender":"M","extra":{"handicap":64,"force":10,"velocity":7,"reaction":0}}]</textarea>
         </form><br/>
         <a href="#" onclick="simulateList()">Simulate</a><br/>
 
